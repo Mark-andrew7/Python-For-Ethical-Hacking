766d9841389006c6497411dad0c5994ded2919a8
Mark-andrew7 <kamaumark19@gmail.com>
2025-06-16 07:57:08 +0000

    Add user input to MAC changer

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,7 +1,7 @@
 import subprocess
 
-interface = "wlan0"
-new_mac = "00:11:22:33:44:55"
+interface = input("interface > ")
+new_mac = input("new mac > ")
 
 subprocess.run("ifconfig "+ interface + " down ", shell=True)
 subprocess.run("ifconfig "+ interface + " hw ether " + new_mac, shell=True)
