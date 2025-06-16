5b6d994debd2cda2f44bb9a375d0d843b0b2d51f
Mark-andrew7 <kamaumark19@gmail.com>
2025-06-16 02:12:20 +0000

    Enhanced MAC changer with variables

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,5 +1,8 @@
 import subprocess
 
-subprocess.run("ifconfig wlan0 down", shell=True)
-subprocess.run("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.run("ifconfig wlan0 up", shell=True)
+interface = "wlan0"
+new_mac = "00:11:22:33:44:55"
+
+subprocess.run("ifconfig "+ interface + " down ", shell=True)
+subprocess.run("ifconfig "+ interface + " hw ether " + new_mac, shell=True)
+subprocess.run("ifconfig "+ interface + " up ", shell=True)
